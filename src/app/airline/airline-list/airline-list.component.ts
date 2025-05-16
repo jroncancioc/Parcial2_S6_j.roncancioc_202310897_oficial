a6998ce6cde83859db38e4a91a36def52791cc59
Juan David Roncancio Camacho <j.roncancioc@uniandes.edu.co>
2025-05-16 14:47:25 -0500

    Primer commit

---
+++ src/app/airline/airline-list/airline-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airline-list',
+  imports: [],
+  templateUrl: './airline-list.component.html',
+  styleUrl: './airline-list.component.css'
+})
+export class AirlineListComponent {
+
+}
